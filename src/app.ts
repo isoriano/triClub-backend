1b6e0e82d07888eb54aa579ace951d2f7e2e2283
Ivan Soriano <ivan.soriano@ersl.ie>
2022-09-23 15:03:44 +0200

    Created production config file

--- src/app.ts
+++ src/app.ts
@@ -5,13 +5,17 @@ import express from 'express';
 import { routes } from './routes';
 import { mongoDbConnect, log as logger } from './utils';
 
-const port = config.get<number>('port');
+const port = config.get<number>('server.port');
+const host = config.get<string>('server.host');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(port, '0.0.0.0', async () => {
+app.listen(port, host, async () => {
+  logger.info(`API is running ar http://${host}:${port}`);
+
   await mongoDbConnect();
+
   routes(app);
 });
