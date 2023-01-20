1f2f865410c4a5c672d1f80a09f36c2f0b6a8967
Ivan Soriano <ivan.soriano@ersl.ie>
2023-01-20 16:42:16 +0100

    Commentes out the different options coded

--- src/services/auth0.service.ts
+++ src/services/auth0.service.ts
@@ -3,12 +3,12 @@ import { ManagementClient, AuthenticationClient } from 'auth0';
 import { auth0Config } from '../config/auth0.config';
 
 export const RequestPasswordChange = async (uid: string) => {
-  const auth = new AuthenticationClient({
-    domain: auth0Config.auth0Domain,
-    clientId: auth0Config.apiMngmtClientId,
-    clientSecret: auth0Config.apiMngmtClientSecret,
-    telemetry: false
-  });
+  // const auth = new AuthenticationClient({
+  //   domain: auth0Config.auth0Domain,
+  //   clientId: auth0Config.apiMngmtClientId,
+  //   clientSecret: auth0Config.apiMngmtClientSecret,
+  //   telemetry: false
+  // });
 
   const auth0 = new ManagementClient({
     domain: auth0Config.auth0Domain,
@@ -17,21 +17,23 @@ export const RequestPasswordChange = async (uid: string) => {
     scope: 'read:users update:users create:user_tickets',
   });
 
-  const user = await auth0.getUser({ id: uid });
-
-  const tr = await auth.changePassword({
-    password: 'Demo_1111!',
-    connection: 'Username-Password-Authentication',
-    email: user.email as string
+  return await auth0.createPasswordChangeTicket({
+    user_id: uid,
   });
 
-  return tr;
+  // const user = await auth0.getUser({ id: uid });
 
-  return await auth.requestChangePasswordEmail({
-    email: 'ivan.soriano.gomez@gmail.com',
-    client_id: auth0Config.apiMngmtClientId,
-    connection: 'con_2',
-  });
+  // return await auth.requestChangePasswordEmail({
+  //   email: 'ivan.soriano.gomez@gmail.com',
+  //   client_id: auth0Config.apiMngmtClientId,
+  //   connection: 'con_2',
+  // });
+
+  // return await auth.requestChangePasswordEmail({
+  //   email: 'ivan.soriano.gomez@gmail.com',
+  //   client_id: auth0Config.apiMngmtClientId,
+  //   connection: 'con_2',
+  // });
 
   // return await auth0.createPasswordChangeTicket({
   //   result_url: 'https://www.google.com',
